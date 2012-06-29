f80532bd721ee95f5ebc322a7ec38cc55476213f
Edwin Luijten <edwin.luijten@live.nl>
2012-06-29 23:37:28 +0200

    Some cleaning up
    Signed-off-by: Edwin Luijten <edwin.luijten@live.nl>

--- routes.php
+++ routes.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__. '/config/config.php';
 
-//Parameter 1: repo
+//Parameter 1: your repository
 //Parameter 2: path to file eg: public-css-style.css
 Route::get('(:bundle)/get/(.*)/(.*)', 'github::repo@get');
--- start.php
+++ start.php
@@ -1,6 +1,7 @@
 <?php
 require_once (__DIR__ . '/libraries/curl.php');
 require_once (__DIR__ . '/libraries/github.php');
+
 Autoloader::namespaces(array(
     'gitHub' => Bundle::path('gitHub').'models',
 ));
