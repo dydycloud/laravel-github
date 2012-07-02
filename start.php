d04c05f92c1feede57ca620d3c34d24c37499767
Edwin Luijten <edwin.luijten@live.nl>
2012-07-02 12:24:57 +0200

    Added markdown helper
    Signed-off-by: Edwin Luijten <edwin.luijten@live.nl>

--- start.php
+++ start.php
@@ -2,6 +2,7 @@
 require_once (__DIR__ . '/libraries/curl.php');
 require_once (__DIR__ . '/libraries/github.php');
 require_once (__DIR__ . '/helpers/chars.php');
+require_once (__DIR__ . '/helpers/markdown.php');
 
 Autoloader::namespaces(array(
     'gitHub' => Bundle::path('gitHub').'models',
