137555e64e1eb778827931239769dec148170b1f
Edwin Luijten <edwin.luijten@live.nl>
2012-07-02 10:48:44 +0200

    Included the helper file
    Signed-off-by: Edwin Luijten <edwin.luijten@live.nl>

--- start.php
+++ start.php
@@ -1,6 +1,7 @@
 <?php
 require_once (__DIR__ . '/libraries/curl.php');
 require_once (__DIR__ . '/libraries/github.php');
+require_once (__DIR__ . '/helpers/chars.php');
 
 Autoloader::namespaces(array(
     'gitHub' => Bundle::path('gitHub').'models',
