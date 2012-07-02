cbfff0391ff245db435aeb9b26613ca3b1d260c7
Edwin Luijten <edwin.luijten@live.nl>
2012-07-02 10:48:23 +0200

    Added a new Route for the new actions
    Signed-off-by: Edwin Luijten <edwin.luijten@live.nl>

--- routes.php
+++ routes.php
@@ -4,3 +4,5 @@ require_once __DIR__. '/config/config.php';
 //Parameter 1: your repository
 //Parameter 2: path to file eg: public-css-style.css
 Route::get('(:bundle)/get/(.*)/(.*)', 'github::repo@get');
+Route::get('(:bundle)/commits/(.*)', 'github::repo@commits');
+Route::get('(:bundle)/repo/(.*)', 'github::repo@repo');
